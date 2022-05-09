4cd23cb31089f60acee27a32ea46b831a36b93ac
Ruben Cepeda <ruben.cepeda@gmail.com>
2022-05-08 23:09:52 -0400

    feat(ColorPickerGradiant): adding a color picker

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,44 @@
   windows_subsystem = "windows"
 )]
 
+use palette::Srgb;
+use palette::Lch;
+use palette::FromColor;
+
+#[tauri::command]
+fn create_gradiant(r: u8, g: u8, b: u8) -> Vec<Vec<u8>> {  
+  let my_rgb = Srgb::new(
+    r as f32 / 255.0,
+    g as f32 / 255.0,
+    b as f32 / 255.0
+  );
+
+  let my_lch = Lch::from_color(my_rgb.into_linear());
+
+  let gradiant = palette::Gradient::new(vec![
+    Lch::new(0.0, my_lch.chroma, my_lch.hue),
+    my_lch,
+    Lch::new(128.0, my_lch.chroma, my_lch.hue),
+  ]);
+  let colors = gradiant
+    .take(10)
+    .map(|color| {
+      let (r,g,b) = Srgb::from_color(color).into_components();
+      vec![
+        (r * 255.0) as u8,
+        (g * 255.0) as u8,
+        (b * 255.0) as u8
+      ]
+    })
+    .collect::<Vec<_>>();
+    return colors;  
+}
+
 fn main() {
-  tauri::Builder::default()
+  tauri::Builder::default()    
+    .invoke_handler(tauri::generate_handler![
+      create_gradiant
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import { useState } from 'react'
 import SketchPicker from 'react-color/lib/components/sketch/Sketch';
+import { invoke } from '@tauri-apps/api/tauri'
 
 function App() {
   const [color, setColor] = useState('#1fa9f4');
+  const [gradiant, setGradiant] = useState([]);
 
   return (
-    <div>
+    <>
       <SketchPicker
         color={color}
         onChange={(color, event) => {
           console.log(color);
-          setColor(color);       
+          setColor(color);
+
+          invoke('create_gradiant', color.rgb)
+            .then((grad) => {
+              setGradiant(grad);
+              console.log(`grad: ${grad}`);
+              console.log(`gradiant: ${gradiant[1]}`);
+            })
+            .catch((e) => console.error(e));
         }}
-       />
-    </div>
+      />
+      {gradiant.map((color, index) => (
+      <div 
+        key={index}
+        style={{
+          padding: '2rem',
+          backgroundColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`
+        }}
+      >
+        rgb({color[0]}, {color[1]}, {color[2]})
+      </div>
+      ))}
+    </>
   )
 }
 
